ccfdd209dc3678e0a95732beb00a527706417709
Aadhi5699 <aadhi5699@gmail.com>
2026-03-18 22:12:54 +0530

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/precise-watertek/',
+  base: '/precise-waterTek/',
   server: {
     host: '0.0.0.0',
     port: 5173,
